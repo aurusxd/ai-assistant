04af18a101db4d0c99ad57283b60d42e61f020c2
aurusxd <159461729+aurusxd@users.noreply.github.com>
2026-08-02 11:07:58 +0700

    feat: add message listener

--- config.py
+++ config.py
@@ -6,3 +6,6 @@ load_dotenv()
 
 DEEPSEEK_API_KEY=os.getenv("DEEPSEEK_API_KEY")
 REDIS_URL = os.getenv("REDIS_URL")
+
+API_ID = os.getenv("API_ID")
+API_HASH = os.getenv("API_HASH")
--- main.py
+++ main.py
@@ -1,24 +1,17 @@
 import asyncio
-import os
 
-from dotenv import load_dotenv
-from hydrogram import Client
+from hydrogram import Client, filters
 
-load_dotenv()
-
-# Replace these with your own values
-api_id = os.getenv("API_ID")
-api_hash = os.getenv("API_HASH")
+from config import API_HASH, API_ID
+from services.llm_service import ask_agent
 
 
 async def main():
-    async with Client("my_account", api_id, api_hash) as app:
-        # Send a message to yourself
-        await app.send_message("me", "Greetings from **Hydrogram**!")
-
-        # Get information about yourself
-        me = await app.get_me()
-        print(f"Successfully logged in as {me.first_name} ({me.id})")
+    async with Client("my_account", API_ID,API_HASH) as app:
+        @app.on_message(filters.text & filters.private)
+        async def echo_handler(client, message):
+            # Формируем ответ и отправляем его
+            await message.reply(ask_agent(message.text, message.from_user.id))
 
 
 asyncio.run(main())
