750d67fb8e2ae0fbc9b8add21e9bb70e8be5d006
aurusxd <159461729+aurusxd@users.noreply.github.com>
2026-08-02 12:01:33 +0700

    logs add

--- main.py
+++ main.py
@@ -1,20 +1,45 @@
-import asyncio
+import logging
 
-from hydrogram import Client, filters, idle
+from hydrogram import Client, filters
 
 from config import API_HASH, API_ID
 from services.llm_service import ask_agent
 
-app = Client("my_account",API_ID,API_HASH)
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s | %(levelname)s | %(message)s",
+)
+
+app = Client(
+    "my_account",
+    api_id=API_ID,
+    api_hash=API_HASH,
+)
+
 
 @app.on_message(filters.text & filters.private)
 async def echo_handler(client, message):
-    if not message.text:
-        return
-    # Формируем ответ и отправляем его
-    await message.reply(await ask_agent(message.text, message.chat.id))
+    logging.info(  # noqa: LOG015
+        "Получено сообщение: chat_id=%s, text=%r",
+        message.chat.id,
+        message.text,
+    )
 
+    if message.outgoing:
+        return
+
+    try:
+        answer = await ask_agent(
+            user_message=message.text,
+            session_id=str(message.chat.id),
+        )
+
+        await message.reply_text(answer)
+
+    except Exception:
+        logging.exception("Ошибка обработки сообщения")
 
 
 if __name__ == "__main__":
+    logging.info("Запускаю Hydrogram")
     app.run()
